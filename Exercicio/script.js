60601718ffafbc07e6669df45b2689a015db41ca
unknown <franciscoaraujo0483@digitalcollege.internal>
2026-01-15 20:30:54 -0300

    Contunação Calculcadora

--- Exercicio/script.js
+++ Exercicio/script.js
@@ -1,12 +1,13 @@
 function soma() {
-    let n1 = number(document.getElementById("num1").value);
-    let n2 = number(document.getElementById("num2").value);
+    let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
 
 
 let resultado = n1 + n2
     console.log("Clicou soma", resultado);
 
-    let strong = document.createElement("strong");
-    strong.innerHTML = "Resultado:" + resultado;
+let resultadoHtml = document.getElementById('Resultado')
+
+    resultadoHtml.innerHTML =  resultado;
 }
 
